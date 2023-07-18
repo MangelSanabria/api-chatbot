61816fd8413b303375ef7d721dacc8872a4cc3ea
mangel <miguel.sanabria@uao.edu.co>
2023-07-18 06:08:11 -0500

    Embedded Data with Common Questions

--- controllers/chatgpt_controller.js
+++ controllers/chatgpt_controller.js
@@ -1,22 +1,103 @@
-const { Configuration, OpenAIApi } = require("openai");
-require("dotenv").config();
+const fs = require("fs");
+const { openai } = require("../utils/helper");
+let embeddedPath = "./embeddedData/embeddedFile.txt";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// Config variables
+let embeddingStore = {};
+const embeds_storage_prefix = "embeds:";
+let embeddedQuestion;
 
-async function new_query(req, res) {
+const createPrompt = (prompt, paragraph) => {
+  return (
+    prompt +
+    "Answer the following questions, also use your own knowledge when necessary :\n\n" +
+    "Context :\n" +
+    paragraph.join("\n\n") +
+    "\n\nAnswer :"
+  );
+};
+
+// Removes the prefix from paragraph
+const keyExtractParagraph = (key) => {
+  return key.substring(embeds_storage_prefix.length);
+};
+
+// Calculates the similarity score of question and context paragraphs
+const compareEmbeddings = (embedding1, embedding2) => {
+  var length = Math.min(embedding1.length, embedding2.length);
+  var dotprod = 0;
+
+  for (var i = 0; i < length; i++) {
+    dotprod += embedding1[i] * embedding2[i];
+  }
+
+  return dotprod;
+};
+
+// Loop through each context paragraph, calculates the score, sort using score and return top count(int) paragraphs
+const findClosestParagraphs = (questionEmbedding, count) => {
+  var items = [];
+
+  for (const key in embeddingStore) {
+    let paragraph = keyExtractParagraph(key);
+
+    let currentEmbedding = JSON.parse(embeddingStore[key]).embedding;
+
+    items.push({
+      paragraph: paragraph,
+      score: compareEmbeddings(questionEmbedding, currentEmbedding),
+    });
+  }
+
+  items.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  return items.slice(0, count).map((item) => item.paragraph);
+};
+
+const new_query = async (req, res) => {
   try {
     const { messages } = req.body;
     console.log(messages);
+
+    // Retrieve embedding store and parse it
+    let embeddingStoreJSON = fs.readFileSync(embeddedPath, {
+      encoding: "utf-8",
+      flag: "r",
+    });
+
+    embeddingStore = JSON.parse(embeddingStoreJSON);
+
+    // Embed the prompt using embedding model
+    let content = messages[messages.length - 1].content;
+
+    let embeddedQuestionResponse = await openai.createEmbedding({
+      input: content,
+      model: "text-embedding-ada-002",
+    });
+
+    // Some error handling
+    if (embeddedQuestionResponse.data.data.length) {
+      embeddedQuestion = embeddedQuestionResponse.data.data[0].embedding;
+    } else {
+      throw Error("Question not embedded properly");
+    }
+
+    // Find the closest count(int) paragraphs
+    let closestParagraphs = findClosestParagraphs(embeddedQuestion, 5);
+    const prompt =
+      "You are Falky helpful assistant Falkensteiner hotels and residences virtual agent, answer in max 3 sentences";
+
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           "role": "system",
-          "content":
-            "You are Falky helpful assistant Falkensteiner hotels and residences virtual agent",
+          "content": createPrompt(
+            prompt,
+            closestParagraphs,
+          ),
         },
         ...messages,
       ],
@@ -28,6 +109,6 @@ async function new_query(req, res) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
 
 module.exports.new_query = new_query;
